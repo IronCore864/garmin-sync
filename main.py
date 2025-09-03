25734800b3a86421a70e9c3ee80c345e6b75a1a3
Tiexin Guo <guotiexin@gmail.com>
2025-09-03 17:07:23 +0800

    chore: fix issues

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from garminconnect import Garmin
+import zipfile
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -16,9 +17,10 @@ cn_client.login()
 logger.info("Garmin CN login successful!")
 cn_client.garth.configure(timeout=30, retries=3)
 
-logger.info("Get latest 3 activities from Garmin CN ...")
-activities = cn_client.get_activities(0, 3)
-logger.info("Get latest 3 activities from Garmin CN done!")
+activities_num = 3
+logger.info(f"Get latest {activities_num} activities from Garmin CN ...")
+activities = cn_client.get_activities(0, activities_num)
+logger.info(f"Get latest {activities_num} activities from Garmin CN done!")
 
 logger.info("Login to Garmin Global ...")
 client = Garmin(
@@ -28,25 +30,31 @@ client.login()
 logger.info("Garmin Global login successful!")
 
 for activity in activities:
-    activity_file = f"./{activity["activityId"]}.fit"
+    activity_file = f"./{activity["activityId"]}.zip"
     try:
         logger.info(
             f"Activity {activity['activityId']}: {activity['activityName']}, started on {activity['startTimeLocal']}"
         )
         logger.info(f"Download activity {activity['activityId']} from Garmin CN ...")
         activity_bytes = cn_client.download_activity(activity["activityId"], dl_fmt=Garmin.ActivityDownloadFormat.ORIGINAL)
-        activity_file = f"./{activity["activityId"]}.fit"
         with open(activity_file, "wb") as f:
             f.write(activity_bytes)
         logger.info(f"Download activity {activity['activityId']} from Garmin CN done!")
 
-        logger.info(f"Upload activity {activity['activityId']} to Garmin Global ...")
-        client.upload_activity(activity_file)
+        with zipfile.ZipFile(activity_file, "r") as z:
+            z.extractall(f"./{activity['activityId']}")
+            unzipped_files = z.namelist()
+        logger.info(f"Upload activity {activity['activityId']}/{unzipped_files[0]} to Garmin Global ...")
+        client.upload_activity(f"./{activity['activityId']}/{unzipped_files[0]}")
         logger.info(f"Upload activity {activity['activityId']} to Garmin Global done!")
     except Exception as e:
         if "Conflict for url" in str(e):
             logger.info("Upload fail, the activity probably already exists.")
             continue
+        logger.info(e)
     finally:
         if os.path.exists(activity_file):
             os.remove(activity_file)
+        for f in unzipped_files:
+            os.remove(f"./{activity['activityId']}/{f}")
+        os.rmdir(f"./{activity['activityId']}")
